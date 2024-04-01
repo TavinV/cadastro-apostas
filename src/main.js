454177434394db9ed9305a93c7df1a1996ef533d
TavinV <otavioviniciusads@gmail.com>
2024-03-31 22:31:17 -0300

    Funcionalidade simples aos botões

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const login = document.getElementById('LOGIN_BUTTON');
+const register = document.getElementById('REGISTER_BUTTON');
+
+login.addEventListener("click", () => {
+    alert("LOGIN")
+})
+
+register.addEventListener("click", () => {
+    alert("REGISTRAR")
+})
