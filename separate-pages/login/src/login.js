40287fa6d5b727e014beeb378f4135eaf781774e
TavinV <otavioviniciusads@gmail.com>
2024-04-10 22:12:08 -0300

    Adicionado funcionalidade no login

---
+++ separate-pages/login/src/login.js
@@ -0,0 +1,39 @@
+const togglePasswordDisplay = document.getElementById("toggle-password-display");
+const passwordDisplayBox = document.getElementById("password-input");
+const passwordIcon = document.getElementById("password-icon");
+
+const hidenPasswordIcon = "eye-outline";
+const exposedPasswordIcon = "eye-off-outline";
+
+var passwordHidden = true;
+
+togglePasswordDisplay.addEventListener('click', () => {
+    if (passwordHidden) {
+        // Caso a senha estiver escondida, iremos mostra-lá e alterar o icone.
+
+        passwordDisplayBox.type = "text";
+        passwordIcon.name = exposedPasswordIcon;
+        passwordHidden = false;
+    }
+    else {
+        // Caso a senha estiver exposta, iremos esconde-lá e alterar o icone.
+
+        passwordDisplayBox.type = "password";
+        passwordIcon.name = hidenPasswordIcon;
+        passwordHidden = true;
+    }
+})
+
+// REALIZANDO A IMPRESSÃO DOS DETALHES DE LOGIN
+
+const loginForm = document.getElementById("login-form");
+const emailTextBox = document.getElementById("email-input");
+const lembrarDeMimInput = document.getElementById("remember-me-input");
+
+loginForm.addEventListener('submit', () => {
+    let passwordGiven = passwordDisplayBox.value;
+    let emailGiven = emailTextBox.value;
+    let lembrarDeMimGiven = lembrarDeMimInput.value;
+
+    alert("LOGIN FEITO: \n Email: " + emailGiven + "\nSenha: " + passwordGiven + "\n Lembrar de mim: " + lembrarDeMimGiven)
+})
--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ const login = document.getElementById('LOGIN_BUTTON');
 const register = document.getElementById('REGISTER_BUTTON');
 
 login.addEventListener("click", () => {
-    window.open("../separate-pages/login.html", '_blank');
+    window.open("../separate-pages/login/login.html", '_blank');
 })
 
 register.addEventListener("click", () => {
-    alert("REGISTRAR")
+    alert("Eloisie volta 😭")
 })
