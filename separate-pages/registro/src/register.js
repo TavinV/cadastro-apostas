36c5a99ccd5611373ef7711fc17d2c3d292b9abe
= <=>
2024-04-11 14:55:35 -0300

    comit 15:00

--- separate-pages/registro/src/register.js
+++ separate-pages/registro/src/register.js
@@ -1,5 +1,6 @@
 const togglePasswordDisplay = document.getElementById("toggle-password-display");
 const passwordDisplayBox = document.getElementById("password-input");
+const passwordDisplayBox2 = document.getElementById("password-input2");
 const passwordIcon = document.getElementById("password-icon");
 
 const hidenPasswordIcon = "eye-outline";
@@ -24,13 +25,30 @@ togglePasswordDisplay.addEventListener('click', () => {
     }
 })
 
+togglePasswordDisplay.addEventListener('click', () => {
+    if (passwordHidden) {
+        // Caso a senha estiver escondida, iremos mostra-lá e alterar o icone.
+
+        passwordDisplayBox.type = "text";
+        passwordIcon.name = exposedPasswordIcon;
+        passwordHidden = false;
+    }
+    else {
+        // Caso a senha estiver exposta, iremos esconde-lá e alterar o icone.
+
+        passwordDisplayBox.type = "password";
+        passwordIcon.name = hidenPasswordIcon;
+        passwordHidden = true;
+    }
+})
+
 // REALIZANDO A IMPRESSÃO DOS DETALHES DE LOGIN
 
-const loginForm = document.getElementById("login-form");
+const registerForm = document.getElementById("register-form");
 const emailTextBox = document.getElementById("email-input");
 const lembrarDeMimInput = document.getElementById("remember-me-input");
 
-loginForm.addEventListener('submit', () => {
+registerForm.addEventListener('submit', () => {
     let passwordGiven = passwordDisplayBox.value;
     let emailGiven = emailTextBox.value;
     let lembrarDeMimGiven = lembrarDeMimInput.value;
