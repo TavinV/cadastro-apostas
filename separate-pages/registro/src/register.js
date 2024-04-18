a86147333ed6aae19bb259b50e96537ad56dbfcb
TavinV <otavioviniciusads@gmail.com>
2024-04-18 17:00:13 -0300

    Comecei os checks do form

--- separate-pages/registro/src/register.js
+++ separate-pages/registro/src/register.js
@@ -41,13 +41,128 @@ togglePasswordDisplay2.addEventListener('click', () => {
     }
 })
 
-// Checar 
+let inputs = {
+    email_input : document.getElementById("email-input"),
+    username_input : document.getElementById("username-input"),
+    password_input : document.getElementById("password-input"),
+    password2_input : document.getElementById("password-input2"),
+    name_input : document.getElementById("name-input"),
+    age_input : document.getElementById("age-input"),
+    phone_input : document.getElementById("phone-input"),
+    cpf_input : document.getElementById("cpf-input"),
+}
 
-template = `
-Hi
-<ion-icon name="warning-outline"></ion-icon>
-`
+// Mensagens de erro
 
-alert("You are my sunshine")
-error = document.getElementById("error-message-email")
-error.innerHTML = template
+let errorMessageEmail = document.getElementById("error-message-email")
+let errorMessageUsername = document.getElementById("error-message-username")
+let errorMessagePassword = document.getElementById("error-message-password")
+let errorMessagePassword2 = document.getElementById("error-message-password2")
+let errorMessageName = document.getElementById("error-message-name")
+let errorMessageAge = document.getElementById("error-message-age")
+let errorMessageTel = document.getElementById("error-message-tel")
+let errorMessageCpf = document.getElementById("error-message-cpf")
+
+// Icones de erro / sucesso
+
+const warningIcon = `<ion-icon name="warning-outline"></ion-icon>`
+const successIcon = `<ion-icon name="checkmark-outline"></ion-icon>`
+const successClass = "login-button"
+
+const register_form = document.getElementById("register-form");
+
+function stringContemTexto(texto1, texto2){
+    return texto2.split('').some(char => texto1)
+}
+
+register_form.addEventListener('submit', (e) => {
+    let erros = 0;
+
+    // Todos os campos estão preenchidos
+
+    for(const [key, value] of Object.entries(inputs)){
+        // alert(key + " : " + value.value);
+
+        if (value.value == " " || value.value == null || value.value == ""){
+           value.innerHTML = "Campo obrigatório!"
+        }
+    }
+
+    // Nome de usuário
+
+    usernamePassed = true
+
+    const prohibitedChars = `/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;~^1234567890`
+    const usernameContemCaracteresProibidos = stringContemTexto(inputs.username_input.value, prohibitedChars)
+
+    if (usernameContemCaracteresProibidos) {
+        erros ++
+        usernamePassed = false
+        errorMessageUsername.innerHTML = "O nome de usuário não pode conter caracteres especiais!" + warningIcon
+    }
+
+    // Caso todos os testes sejam positivos 
+    if (usernamePassed) {
+        errorMessageUsername.innerHTML = successIcon
+        errorMessageUsername.setAttribute("class", successClass)
+    }
+
+    // Senha 
+
+    passwordPassed = true
+
+    const specialChars = `/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;`
+    const numbers =  `1234567890`
+
+    const senhaContemCaracterEspecial = stringContemTexto(inputs.password_input.value, specialChars)
+    const senhaContemNumeros = stringContemTexto(inputs.password_input.value, numbers)
+
+    if (!senhaContemCaracterEspecial) {
+        erros++
+        passwordPassed = false
+        errorMessagePassword.innerHTML = "Senha deve conter ao menos um caracter especial!" + warningIcon
+    }
+    if (!senhaContemNumeros) {
+        erros++
+        passwordPassed = false
+        errorMessagePassword.innerHTML = "Senha deve conter ao menos um numero!" + warningIcon
+    }
+
+    // Caso todos os testes sejam positivos 
+
+    if (passwordPassed) {
+        errorMessagePassword.innerHTML = successIcon
+        errorMessagePassword.setAttribute("class", successClass)
+    }
+
+
+    // Senhas batem
+
+    password2Passed = true
+
+    if (inputs.password_input.value != inputs.password2_input.value){
+        erros++
+        password2Passed = false
+        errorMessagePassword2.innerHTML = "As senhas não coincidem!" + warningIcon
+    }
+
+    // Caso todos os testes sejam positivos 
+    if (password2Passed) {
+        errorMessagePassword2.innerHTML = successIcon
+        errorMessagePassword2.setAttribute("class", successClass)
+    }
+
+    // Nome completo
+
+    if (inputs.name_input.value.split(" ").lenght < 1) {
+        // Caso o nome completo seja pelo menos um nome e sobrenome
+
+
+    }
+
+    // Impedir o envio do formulario 
+
+    if (erros > 0){
+        e.preventDefault()
+    }
+})
